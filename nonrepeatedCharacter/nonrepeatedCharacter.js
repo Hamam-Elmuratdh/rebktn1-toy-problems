fd855d859cc5a4e7bb405ae98720db654302c3f9
HamamEL <hamam.alsaiity@gmail.com>
2019-11-25 00:40:54 -0800

    finally worked!

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,17 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
-};
+  var count = {}; 
+  for ( var i=0; i<string.length; i++){
+  	if (count[string[i]]){
+  		count[string[i]]++;
+  	}else{
+  		count[string[i]]= 1;
+  	}
+  }
+  for ( var i =0; i<string.length; i++){
+  	if ( count[string[i]] ===1){
+  		return string[i];
+  	}
+  	}
+}
