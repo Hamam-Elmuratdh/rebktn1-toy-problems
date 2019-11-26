996fad0b5c58d22854d805bf3a2ac6874f3ff5c8
HamamEL <hamam.alsaiity@gmail.com>
2019-11-26 00:37:04 -0800

    Black Jack

--- blackjackScorer/blackjack.js
+++ blackjackScorer/blackjack.js
@@ -5,7 +5,8 @@ Scoring rules:
 
 Number cards count as their face value (2 through 10). Jack, Queen and King count as 10. An Ace can be counted as either 1 or 11.
 
-Return the highest score of the cards that is less than or equal to 21. If there is no score less than or euqal to 21 return the smallest score more than 21.
+Return the highest score of the cards that is less than or equal to 21.
+ If there is no score less than or euqal to 21 return the smallest score more than 21.
 Examples
 
 ["A"]                           ==>  11
@@ -13,3 +14,15 @@ Examples
 ["A", "10", "A"]                ==>  12
 ["5", "3", "7"]                 ==>  15
 ["5", "4", "3", "2", "A", "K"]  ==>  25 */
+function blackJack(array){
+	var count = 0;
+	var result = []
+	for (var i = 0; i < array.length; i ++){
+		if (array [i] = 'Q' || array[i] = 'K'|| arra[i] = 'J')
+			count += 10;
+}
+//if A equals the same element in the iput push A to result
+// itterate again over the array if the count equal or less than 21 we will add 11 to it 
+// else we will add 1 
+//return count
+}
