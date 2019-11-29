4421a35b8a0e1ef6b31014668bd65262d3adf3a2
HamamEL <hamam.alsaiity@gmail.com>
2019-11-29 00:20:45 -0800

    done

--- cheaterFriend/cheateFriend.js
+++ cheaterFriend/cheateFriend.js
@@ -1,10 +1,12 @@
 
 //     A friend of mine takes a sequence of numbers from 1 to n (where n > 0).
 //     Within that sequence, he chooses two numbers, a and b.
-//     He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+//     He says that the product of a and b should be equal to the sum of all numbers in the sequence,
+// excluding a and b.
 //     Given a number n, could you tell me the numbers he excluded from the sequence?
 
-// The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+// The function takes the parameter: n (n is always strictly greater than 0) 
+//and returns an array or a string (depending on the language) of the form:
 
 // [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
 
@@ -12,8 +14,21 @@
 
 // [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ...will be sorted in increasing order of the "a".
 
-// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) 
+//if no possible numbers are found which will prove that my friend has not told the truth! 
+//(Go: in this case return nil).
 //
 //
 // Example:
 // removNb(26) should return [ {15, 21}, {21, 15} ]
+function find (n){
+	var product = 0;
+	var result = [];
+	var a;
+	var b;
+	var sum = 0;
+	for (var i =0; i<n.length; i++){
+		sum += i;
+	}
+	// 
+
