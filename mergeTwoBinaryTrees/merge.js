240cf92e430f88085146a1060f365c59e939bbf4
Hamam-Elmuratdh <hamam.alsaiity@gmail.com>
2019-12-11 00:53:02 -0800

    unfinished

--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -1,7 +1,10 @@
 /*
-Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+Given two binary trees and imagine that when you put one of them to cover the other,
+// some nodes of the two trees are overlapped while the others are not.
 
-You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap,
+// then sum node values up as the new value of the merged node. Otherwise, 
+//the NOT null node will be used as the node of new tree.
 
 Example 1:
 
@@ -24,16 +27,26 @@ Merged tree:
 Note: The merging process must start from the root nodes of both trees.
 */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} t1
- * @param {TreeNode} t2
- * @return {TreeNode}
- */
-var mergeTrees = function(t1, t2) {};
+
+ //Definition for a binary tree node.
+  function TreeNode(val) {
+      this.val = val;
+      this.left = this.right = null;
+  }
+ 
+
+  @param {TreeNode} t1
+  @param {TreeNode} t2
+  @return {TreeNode}
+ 
+var mergeTrees = function(t1, t2) {
+	var node = newTreenode;
+	if ( t1 === null )
+		return t2 
+	if (t2 === null)
+		return t1
+	//
+	//
+	//
+
+};
