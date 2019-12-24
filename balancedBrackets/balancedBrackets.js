ca644df2648724f80165e83820fe322aa6705b70
Hamam-Elmuratdh <hamam.alsaiity@gmail.com>
2019-12-24 00:22:38 -0800

    notfinished

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -1,6 +1,7 @@
 /*
 Balanced Brackets
-Given a string, return true if its arrangement of parenthesis (), curly-brackets {}, and square-brackets [] would be considered valid in code and mathematics (is balanced).
+Given a string, return true if its arrangement of parenthesis (), curly-brackets {}, and square-brackets [] 
+\\would be considered valid in code and mathematics (is balanced).
 
 isBalanced("(x + y) - (4)") // true
 isBalanced("(((10 ) ()) ((?)(:)))") // true
@@ -9,4 +10,30 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+	var count = 0; 
+	 for (var i = 0; i < str.length; i++) {
+	 	//var bracket = str.char(i)
+  	if (count < 0) {
+      return false;
+    } else if (str[i] === '(') {
+      count++;
+    } else if (str[i] === ')') {
+      count--;
+    }
+    else if (str[i] === '{') {
+      count++;
+    } else if (str[i] === '}') {
+      count--;
+    }else if (str[i] === '[' ) {
+      count++;
+    } else if (str[i] === ']') {
+      count--;
+    }
+  }
+ 
+  	if (count === 0) {
+   return true;
+}
+  return false;
+};
