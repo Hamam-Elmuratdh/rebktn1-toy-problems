b0cea3739316f2637a79b886ef9228a898302e75
Hamam-Elmuratdh <hamam.alsaiity@gmail.com>
2020-01-03 10:40:50 +0200

    not finished

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -1,7 +1,8 @@
 /*
 Longest Run
 
-Write a function that, given a string, finds the longest run of identical characters and returns an array containing the start and end indices of that run. 
+Write a function that, given a string, finds the longest run of identical characters and returns an array containing
+ the start and end indices of that run. 
 If there are two runs of equal length, return the first one. Return [0,0] for no runs.
 
 longestRun("abbbcc") // [ 1, 3 ]
@@ -12,4 +13,15 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string) {
+    var result = [];
+    var counter = 0;
+    string = string.split( '')
+    for (var i= 0 ; i<string.length; i++){
+      if (string[i].valueOf === string[i].valueOf()){
+       counter++
+          result.push(string[i])
+      }
+    }
+    return result
+}
