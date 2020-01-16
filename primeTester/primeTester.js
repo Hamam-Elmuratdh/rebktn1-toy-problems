e487036363857c7dd62d21b46011987f9fe02101
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-16 09:16:08 +0100

    solved

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,4 +9,18 @@ primeTester(1) // false
 
 */
 
-function primeTester(n) {}
+function primeTester(n) {
+  
+    if ( n === 1|| n < 1 || n % 1 !== 0) {
+        return false;
+      }
+    
+      for ( var i = 2; i < n; i++ ) {
+        if ( n % i === 0 ) {
+          return false;
+        }
+      }
+    
+      return true;
+
+}
