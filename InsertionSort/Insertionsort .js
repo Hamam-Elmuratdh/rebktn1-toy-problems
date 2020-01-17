58e916f2c40b427f909ad41cf4be6c0f3a6adc45
Hamam-Elmurardh <mohamedbel@protonmail.com>
2020-01-17 09:55:32 +0100

    Not solved

--- InsertionSort/Insertionsort .js
+++ InsertionSort/Insertionsort .js
@@ -2,7 +2,8 @@
 // Algorithm of Insertion Sort:
 
 //1- Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list.
-//2- At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there.
+//2- At each iteration, insertion sort removes one element from the input data,
+// finds the location it belongs within the sorted list, and inserts it there.
 // 3-It repeats until no input elements remain.
 
 // Example 1:
@@ -14,15 +15,17 @@
 // Input: -1->5->3->4->0
 // Output: -1->0->3->4->5
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
+
+ //Definition for singly-linked list.
+ function ListNode(val) {
+     this.val = val;
+     this.next = null;
+ }
+// */
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var insertionSortList = function(head) {};
+var insertionSortList = function(head) {
+
+};
